e34f8a7d2d83293c2679907def71c676593e21a9
David Zarlengo <dzarlengo@simplymeasured.com>
2013-09-22 15:43:28 -0700

    bump version to 1.1.0

--- version.go
+++ version.go
@@ -1,5 +1,17 @@
 package main
 
 const (
-	VERSION = "1.0.0"
+	/*
+	   CHANGES
+
+	   1.1.0:
+	     * added a second MongoDB experiment "writes" for writing
+	       random-ish documents of a fixed length to the database
+
+	   1.0.0:
+	     * initial drop containing the "counters" experiment for
+	       MongoDB which performs increments of counters on a
+	       single document
+	*/
+	VERSION = "1.1.0"
 )
