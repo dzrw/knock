02b9828b3d6a362300cd97665930e9852941de1e
David Zarlengo <dzarlengo@simplymeasured.com>
2013-09-22 20:22:46 -0700

    add the internals.OpsPerStall property to help deal with measurement issues with really zippy Work calls (such as MongoDB with writeConcern=unsafe)

--- sandbox.go
+++ sandbox.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"errors"
 	_ "launchpad.net/tomb"
 	"log"
+	_ "math"
+	"strconv"
 	"sync"
 	"time"
 )
@@ -18,26 +21,29 @@ type SandboxInfo struct {
 }
 
 type sandbox struct {
-	id       int
-	props    map[string]string
-	d        time.Duration
-	start    time.Time
-	emitter  LatencyEmitter
-	wg       *sync.WaitGroup
-	behavior Behavior
-	factory  BehaviorFactory
+	id            int
+	props         map[string]string
+	d             time.Duration
+	start         time.Time
+	emitter       LatencyEmitter
+	wg            *sync.WaitGroup
+	behavior      Behavior
+	factory       BehaviorFactory
+	stall         bool
+	opsPerStall   int
+	stall_counter int
+	stalls        int
 }
 
 func NewSandbox(info *SandboxInfo) *sandbox {
 	return &sandbox{
-		info.Id,
-		info.Properties,
-		info.Duration,
-		info.StartTime,
-		info.Emitter,
-		info.WaitGroup,
-		nil,
-		info.Factory,
+		id:      info.Id,
+		props:   info.Properties,
+		d:       info.Duration,
+		start:   info.StartTime,
+		emitter: info.Emitter,
+		wg:      info.WaitGroup,
+		factory: info.Factory,
 	}
 }
 
@@ -63,6 +69,11 @@ func (this *sandbox) loop() {
 }
 
 func (this *sandbox) setup() {
+	err := this.parseProperties()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	res, err := this.init()
 	if err != nil {
 		log.Fatal(err)
@@ -114,7 +125,24 @@ func (this *sandbox) work() (err error) {
 
 	t0 := time.Now()
 	res := this.behavior.Work(t0)
-	usec := int64(time.Since(t0) / time.Microsecond)
+	d := time.Since(t0)
+	usec := int64(d / time.Microsecond)
+
+	// If stalling is enabled, then introduce a 1s pause
+	// every N ops. Hopefully, this will fix the issue with
+	// some Work operations completing far too fast for
+	// the progress/stats reporting goroutines to work.
+	// I'm not sure why the select{} construct isn't picking
+	// the other channels...
+	if this.stall {
+		this.stall_counter += 1
+
+		if this.stall_counter > this.opsPerStall {
+			this.stalls += 1
+			this.stall_counter = 0
+			<-time.After(1 * time.Second)
+		}
+	}
 
 	this.emitter.PublishResponseTime(this.id, usec, res)
 	return
@@ -139,5 +167,27 @@ func (this *sandbox) close() (err error) {
 
 	this.behavior.Close()
 	this.behavior = nil
+	return
+}
+
+func (this *sandbox) parseProperties() (err error) {
+	props := this.props
+
+	const (
+		MinOpsPerStall = 10000
+	)
+
+	if v, ok := props["internals.OpsPerStall"]; ok {
+		u, err := strconv.Atoi(v)
+		if err != nil || u < MinOpsPerStall {
+			return errors.New("internals.OpsPerStall must be >= 10000 ops (e.g. don't use this option when response times are slow enough to measure)")
+		}
+
+		this.stall = true
+		this.opsPerStall = u
+	} else {
+		this.stall = false
+	}
+
 	return
 }
--- sandbox_test.go
+++ sandbox_test.go
@@ -1,7 +1,9 @@
 package main
 
 import (
-	_ "log"
+	"log"
+	_ "math"
+	"strconv"
 	"sync"
 	"testing"
 	"time"
@@ -44,4 +46,96 @@ func TestSandbox(t *testing.T) {
 		t.Errorf("This test should have taken at least %d seconds.", 15)
 		return
 	}
+}
+
+func TestSandboxWithMinWorkTime(t *testing.T) {
+	const (
+		TestDuration  = 5 * time.Second
+		OpsPerStall   = 10000
+		SleepDuration = 5 * time.Microsecond
+		StallDuration = 1 * time.Second
+	)
+
+	if testing.Short() {
+		t.Skip("skipping test in short mode.")
+	}
+
+	log.Printf("Running a %.2f second test...", TestDuration.Seconds())
+
+	t0 := time.Now()
+
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+
+	tm := NewTaskMaster(&TaskMasterInfo{
+		WaitGroup:  wg,
+		Properties: make(map[string]string),
+	})
+	tm.Start()
+
+	sb := NewSandbox(&SandboxInfo{
+		Id: 1,
+		Properties: map[string]string{
+			"mongodb.run":           "counters",
+			"mongodb.url":           "mongodb://localhost:27017",
+			"internals.OpsPerStall": strconv.Itoa(OpsPerStall),
+		},
+		Duration:  TestDuration,
+		StartTime: t0,
+		Emitter:   tm,
+		WaitGroup: wg,
+		Factory: func() Behavior {
+			return &dummy_behavior{
+				sleep: SleepDuration,
+			}
+		},
+	})
+
+	sb.Start()
+
+	ops := countResponseTimes(tm)
+
+	if time.Since(t0) < TestDuration {
+		t.Errorf("This test should have taken at least %.1f seconds.", TestDuration.Seconds())
+		return
+	}
+
+	// When the response times are very small (on the order of nanoseconds),
+	// then other channels aren't serviced as fairly by the Go scheduler
+	// (I think -- I'm certainly not seeing code associated with some channels
+	// run (e.g. IO)).  At those response times, the millisecond stall interval
+	// should dominate the ops/sec.  That is, performing OpsPerStall ops should
+	// take far far less time that performing the stall.  If we through out that
+	// term, then we can estimate the # of stalls, and put an upper bound on the
+	// number of ops.
+	expected_stalls := float64(TestDuration.Seconds()) / float64(StallDuration.Seconds())
+	if float64(sb.stalls) < expected_stalls {
+		t.Errorf("expected at least: %.4f stalls, observed: %d stalls",
+			expected_stalls, sb.stalls)
+		return
+	}
+
+	// Add a 1% fudge factor to the upper bound because of error
+	// in the measurement of the actual duration of the test.
+	expected_max_ops := float64(OpsPerStall) * expected_stalls * 1.01
+	if float64(ops) > expected_max_ops {
+		t.Errorf("expected at most: %.4f ops, observed: %d ops",
+			expected_max_ops, ops)
+		return
+	}
+}
+
+type dummy_behavior struct {
+	sleep time.Duration
+}
+
+func (*dummy_behavior) Init(props map[string]string) (err error) {
+	return
+}
+
+func (*dummy_behavior) Close() {}
+
+func (this *dummy_behavior) Work(t0 time.Time) (res WorkResult) {
+	<-time.After(this.sleep)
+	return WRK_OK
 }
